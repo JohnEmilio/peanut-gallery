ddfe02f8332b1107ec7498f90b3b47496f85a2a6
Gabe <gabrielarce3@gmail.com>
2022-09-03 02:07:03 -0700

    add partials header ejs, change styling, add materialize into the mix

--- public/js/main.js
+++ public/js/main.js
@@ -3,26 +3,26 @@ const reviewItem = document.querySelectorAll('span.not')
 const reviewComplete = document.querySelectorAll('span.completed')
 
 
-Array.from(deleteBtn).forEach((el)=>{
+Array.from(deleteBtn).forEach((el) => {
     el.addEventListener('click', deleteReview)
 })
 
-Array.from(reviewItem).forEach((el)=>{
+Array.from(reviewItem).forEach((el) => {
     el.addEventListener('click', markComplete)
 })
 
-Array.from(reviewComplete).forEach((el)=>{
+Array.from(reviewComplete).forEach((el) => {
     el.addEventListener('click', markIncomplete)
 })
 
 
 
-async function deleteReview(){
+async function deleteReview() {
     const reviewId = this.parentNode.dataset.id
-    try{
+    try {
         const response = await fetch('./deleteReview', {
             method: 'delete',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'reviewIdFromJSFile': reviewId
             })
@@ -30,17 +30,17 @@ async function deleteReview(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markComplete(){
+async function markComplete() {
     const todoId = this.parentNode.dataset.id
-    try{
+    try {
         const response = await fetch('todos/markComplete', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'todoIdFromJSFile': todoId
             })
@@ -48,17 +48,17 @@ async function markComplete(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markIncomplete(){
+async function markIncomplete() {
     const todoId = this.parentNode.dataset.id
-    try{
+    try {
         const response = await fetch('todos/markIncomplete', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'todoIdFromJSFile': todoId
             })
@@ -66,7 +66,31 @@ async function markIncomplete(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
+
+window.addEventListener('load', (event) => {
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.ml2');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({ loop: true })
+        .add({
+            targets: '.ml2 .letter',
+            scale: [4, 1],
+            opacity: [0, 1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 950,
+            delay: (el, i) => 70 * i
+        }).add({
+            targets: '.ml2',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
+
+});
